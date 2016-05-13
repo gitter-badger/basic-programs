545819e0e52823de89dd76fccadda725b3aa5a3d
Aditya Ultra <adityaultra4@gmail.com>
2016-05-13 14:49:57 +0530

    Better file handler, with parser, Insert fxn not working :sad:

--- file-handling/file-bin-h1.cpp
+++ file-handling/file-bin-h1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 
-#include <string>
+#include <cstring>
 
 using namespace std;
 
@@ -9,30 +9,36 @@ int id = 0;
 
 struct student{
     int id;
+    char name[40];
     int marks;
-    string name;
 };
+
+void refresh() {
+    ofstream file("data.dat", ios::trunc|ios::binary);
+    file.close();
+}
 
 void read() {
     student ob1;
     ifstream file("data.dat", ios::in | ios::binary);
 
     while ( file.read((char*)&ob1, sizeof(ob1)) ) {
-        cout << "Name = " << ob1.name << endl;
         cout << "Id = " << ob1.id << endl;
+        cout << "Name = " << ob1.name << endl;
         cout << "Aggregate marks = " << ob1.marks << endl;
     }
 
     file.close();
 }
 
-void search(string name){
+void search(char name[40]){
+    // search for the given name
     student ob1;
     ifstream file("data.dat", ios::in | ios::binary);
 
     while ( file.read((char*)&ob1, sizeof(ob1)) ) {
-        if(name == ob1.name) {
-            cout << "Name = " << ob1.name << endl;
+        if(strcmp(name, ob1.name)==0) {
+            cout << "\nName = " << ob1.name << endl;
             cout << "Id = " << ob1.id << endl;
             cout << "Aggregate marks = " << ob1.marks << endl;
         }
@@ -47,49 +53,78 @@ void append(student ob) {
     file.close();
 }
 
-void insert(student ob2) {
+void insert(student ob2) { // This doesn't work :(
     student ob1;
-    ifstream file("data.dat", ios::in | ios::binary);
-    ofstream temp("temp.dat", ios::out | ios::binary);
+    fstream file("data.dat", ios::binary);
+
+    int ws = 0;
 
     while ( file.read((char*)&ob1, sizeof(ob1)) ) {
-        if (ob1.id < ob2.id) {
-            temp.write((char*)&ob1, sizeof(ob1));
+        if (ob1.id == ob2.id) {
+            ws++;
+            file.seekg(-sizeof(ob2), ios::cur);
+            file.write((char*)&ob2, sizeof(ob2));
         }
 
-        else if (ob1.id == ob2.id) {
-            temp.write((char*)&ob2, sizeof(ob2));
-        }
-
-        else {
+        else if (ob1.id > ob2.id && ws) {
             ob1.id++;
-            temp.write((char*)&ob1, sizeof(ob1));
+            file.write((char*)&ob1, sizeof(ob1));
         }
     }
 
     file.close();
-    temp.close();
-
-    remove("data.dat");
-    rename("temp.dat", "data.dat");
 }
 
 void input() {
     student ob1;
-    cout << "Enter the Name" << endl;
-    getline(cin, ob1.name);
-    cout << "Enter Aggregate marks" << endl;
+    cout << "Enter the Name :" << endl;
+    cin.getline(ob1.name, 40);
+    cout << "Enter Aggregate marks :" << endl;
     cin >> ob1.marks;
     ob1.id = ++id;
 
     append(ob1);
 }
 
+void parser(){
+    char command[40];
+
+    while ( strcmp(command, "q") ){
+        cout << "\n*> ";
+
+        cin.getline(command, 40);
+
+        if (strcmp(command, "new") == 0) {
+            input();
+            
+        }
+
+        else if (strcmp(command, "search") == 0) {
+            char query[40];
+            cout << "Enter Name to search > ";
+            cin.getline(query, 40);
+            search(query);
+        }
+
+        else if (strcmp(command, "display") == 0) {
+            read();
+        }
+
+        else if (strcmp(command, "delete") == 0) {
+            char confirm;
+            cout << "Enter y to confirm > ";
+            cin >> confirm;
+            if (confirm == 'y') {
+                refresh();
+            }
+        }
+
+    }
+}
+
 int main() {
 
-    input();
-
-    read();
+    parser();
 
     return 0;
 }
--- file-handling/file-h1.cpp
+++ file-handling/file-h1.cpp
@@ -3,8 +3,7 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     ofstream file("myFile.txt"); // open file
 
     file << "Cool This writes to file" << endl;
